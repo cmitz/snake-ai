ffedfdd6f44421a08229087fb754ab1c38e4df72
Casper Smits <casper@caspersmits.nl>
2018-01-27 16:50:09 +0100

    Get rid of the text

--- agent.py
+++ agent.py
@@ -3,11 +3,11 @@ from move import Move, Direction
 
 import math
 
-bias_g_weight = 10
+bias_g_weight = 5
 bias_snake_body = 1000000
 bias_snake_body_adjacent = -20
-bias_less_corners = -10
-bias_wall_adjacent = -25
+bias_less_corners = -2
+bias_wall_adjacent = -2
 
 
 class Agent:
@@ -21,41 +21,6 @@ class Agent:
         self.board = None
 
     def get_move(self, board, score, turns_alive, turns_to_starve, direction):
-        """This function behaves as the 'brain' of the snake. You only need to change the code in this function for
-        the project. Every turn the agent needs to return a move. This move will be executed by the snake. If this
-        functions fails to return a valid return (see return), the snake will die (as this confuses its tiny brain
-        that much that it will explode). The starting direction of the snake will be North.
-
-        :param board: A two dimensional array representing the current state of the board. The upper left most
-        coordinate is equal to (0,0) and each coordinate (x,y) can be accessed by executing board[x][y]. At each
-        coordinate a GameObject is present. This can be either GameObject.EMPTY (meaning there is nothing at the
-        given coordinate), GameObject.FOOD (meaning there is food at the given coordinate), GameObject.WALL (meaning
-        there is a wall at the given coordinate. TIP: do not run into them), GameObject.SNAKE_HEAD (meaning the head
-        of the snake is located there) and GameObject.SNAKE_BODY (meaning there is a body part of the snake there.
-        TIP: also, do not run into these). The snake will also die when it tries to escape the board (moving out of
-        the boundaries of the array)
-
-        :param score: The current score as an integer. Whenever the snake eats, the score will be increased by one.
-        When the snake tragically dies (i.e. by running its head into a wall) the score will be reset. In ohter
-        words, the score describes the score of the current (alive) worm.
-
-        :param turns_alive: The number of turns (as integer) the current snake is alive.
-
-        :param turns_to_starve: The number of turns left alive (as integer) if the snake does not eat. If this number
-        reaches 1 and there is not eaten the next turn, the snake dies. If the value is equal to -1, then the option
-        is not enabled and the snake can not starve.
-
-        :param direction: The direction the snake is currently facing. This can be either Direction.NORTH,
-        Direction.SOUTH, Direction.WEST, Direction.EAST. For instance, when the snake is facing east and a move
-        straight is returned, the snake wil move one cell to the right.
-
-        :return: The move of the snake. This can be either Move.LEFT (meaning going left), Move.STRAIGHT (meaning
-        going straight ahead) and Move.RIGHT (meaning going right). The moves are made from the viewpoint of the
-        snake. This means the snake keeps track of the direction it is facing (North, South, West and East).
-        Move.LEFT and Move.RIGHT changes the direction of the snake. In example, if the snake is facing north and the
-        move left is made, the snake will go one block to the left and change its direction to west.
-        """
-
         if len(self.current_route) > 0:
             return self.current_route.pop(0)
 
@@ -72,11 +37,6 @@ class Agent:
             return Move.STRAIGHT
 
     def on_die(self):
-        """This function will be called whenever the snake dies. After its dead the snake will be reincarnated into a
-        new snake and its life will start over. This means that the next time the get_move function is called,
-        it will be called for a fresh snake. Use this function to clean up variables specific to the life of a single
-        snake or to host a funeral.
-        """
         self.current_position = None
         self.food_positions = []
         self.current_goal = None
@@ -108,6 +68,9 @@ class Agent:
 
         route = self.a_star(current_position, goal, start_direction)
         total_cost = 0
+
+        if route is None:
+            return []
 
         route_elements = [route.position]
         while route.parent is not None:
@@ -150,6 +113,8 @@ class Agent:
             nodes_expanded += 1
 
             for child_node in self.adjacent_nodes(current_node):
+                if self.blocked_field(child_node.game_object):
+                    break
                 child_node = SearchNode(child_node.position, current_node, child_node
                                         .game_object, child_node.direction)
 
@@ -173,15 +138,13 @@ class Agent:
 
             closed_list.append(current_node)
 
-        print(f"A* completed, no route found;")
-
     def calculate_heuristic(self, child_node, goal):
         bias = 0
         game_object = child_node.game_object
         if game_object == GameObject.SNAKE_HEAD \
                 or game_object == GameObject.SNAKE_BODY \
                 or game_object == GameObject.WALL:
-            print(f"What the hell dude, thats a wall! {child_node.position}")
+            print(f"What the hell dude, that's a wall! {child_node.position}")
             bias += bias_snake_body
 
         adjacent_nodes = self.adjacent_nodes(child_node)
